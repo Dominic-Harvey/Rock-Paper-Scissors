a9aea12710c79fa95fd3f95a0e7d1708edaacdd8
Dominic-Harvey <66623504+Dominic-Harvey@users.noreply.github.com>
2020-06-24 13:18:01 +0100

    added R, P and S as valid inputs

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -2,20 +2,32 @@ import random
 players_throws = []
 winner = [0]
 
+choices={
+       'r': 'rock',
+       'p': 'paper',
+       's':  'scissors',
+       'rock': 'rock',
+       'paper': 'paper',
+       'scissors': 'scissors',
+       1 : 'rock',
+       2 : 'paper',
+       3 : 'scissors'
+    }
+
 def play(winner):
     player_choice = input("Rock Paper or Scissors? ").lower()
     bot_choice = throw_predictor(winner[-1], players_throws)
-    print (f"{player_choice} VS {bot_choice}")
-    if player_choice == bot_choice:
+    print (f"{choices[player_choice]} VS {bot_choice}")
+    if choices[player_choice] == bot_choice:
         print('Tie!')
         winner.append(0)    
-    elif compare(player_choice, bot_choice):
+    elif compare(choices[player_choice], bot_choice):
         print('Player Wins!')
         winner.append(1)
     else:
         print('Computer Wins!')
         winner.append(-1)
-    players_throws.append(player_choice.lower())
+    players_throws.append(choices[player_choice].lower())
     return winner
 
 
@@ -38,17 +50,15 @@ def opposite_throw(players_throws):
 
 
 def throw_predictor(winner, players_throws):
-    choices = {1 : 'rock', 2 : 'paper', 3 : 'scissors'}
     winner = str(winner)
     if int(winner[-1]) == 1: 
         smart_bot_choice = opposite_throw(players_throws[-1])
-        print (smart_bot_choice)
     elif int(winner[-1]) == -1:
-        smart_bot_choice = players_throws[-1]
-        print (smart_bot_choice)    
+        smart_bot_choice = players_throws[-1] 
     elif int(winner[-1]) == 0:
         smart_bot_choice = choices[random.randint(1,3)]
     return smart_bot_choice
+
 
 rounds = int(input("How many rounds would you like to play? "))
 for _ in range(rounds):
