9f8a2d3431bd65bc3a1e306f100bfe2c2c6b6d87
Dominic-Harvey <66623504+Dominic-Harvey@users.noreply.github.com>
2020-06-24 14:34:02 +0100

    corrected throw predictor algo

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,6 +1,6 @@
 import random
 players_throws = []
-winner = [0]
+players_results = []
 
 choices={
        'r': 'rock',
@@ -14,21 +14,21 @@ choices={
        3 : 'scissors'
     }
 
-def play(winner):
+def play(players_results):
     player_choice = input("Rock Paper or Scissors? ").lower()
-    bot_choice = throw_predictor(winner[-1], players_throws)
+    bot_choice = throw_predictor(players_results, players_throws)
     print (f"{choices[player_choice]} VS {bot_choice}")
     if choices[player_choice] == bot_choice:
         print('Tie!')
-        winner.append(0)    
+        players_results.append('tie')    
     elif compare(choices[player_choice], bot_choice):
         print('Player Wins!')
-        winner.append(1)
+        players_results.append('win')
     else:
         print('Computer Wins!')
-        winner.append(-1)
-    players_throws.append(choices[player_choice].lower())
-    return winner
+        players_results.append('loss')
+    return players_throws.append(choices[player_choice].lower())
+   # players_results
 
 
 def compare(playerChoice, botChoice):
@@ -40,30 +40,35 @@ def compare(playerChoice, botChoice):
                ('scissors','rock') : False}
     return results[(playerChoice, botChoice)]
 
-def opposite_throw(players_throws):
+def opposite_throw(last_throw):
     opposite_throw = {
         'paper': 'scissors',
         'rock': 'paper',
         'scissors': 'rock'       
     }
-    return opposite_throw[players_throws]
+    return opposite_throw[last_throw]
 
 
-def throw_predictor(winner, players_throws):
-    winner = str(winner)
-    if int(winner[-1]) == 1: 
-        smart_bot_choice = opposite_throw(players_throws[-1])
-    elif int(winner[-1]) == -1:
-        smart_bot_choice = players_throws[-1] 
-    elif int(winner[-1]) == 0:
+def throw_predictor(players_results, players_throws):
+    print (players_throws)
+    print (players_results)
+    try:
+        if players_results[-1] == 'loss' and players_results[-2] == 'loss':
+            smart_bot_choice = players_throws[-1]
+            print ('using loss algo')
+        elif players_results[-1] == 'win' or players_results[-2] == 'win' and players_results[-1] == 'win':
+            smart_bot_choice = opposite_throw(players_throws[-1])
+            print ('using win algo') 
+        return smart_bot_choice
+    except:
         smart_bot_choice = choices[random.randint(1,3)]
-    return smart_bot_choice
+        print ('using else calc')
+        return smart_bot_choice
 
 
 rounds = int(input("How many rounds would you like to play? "))
 for _ in range(rounds):
-    play(winner)
-    score = (sum(x > 0 for x in winner))
-    
-print (f"You won {score} out of {rounds}")
-final = input("Press any key to close")
+    play(players_results)
+   
+   
+final = input("Press enter to close")
