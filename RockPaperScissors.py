2061db24dc3d05bf3b03fcc209cc9002c28cf85a
Dominic-Harvey <66623504+Dominic-Harvey@users.noreply.github.com>
2020-06-29 15:53:28 +0100

    implemented improved prediction algo

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -23,31 +23,32 @@ def play():
         return player_choice
 
     player_choice = ask_choice()
-    bot_choice = throw_predictor(players_results, players_throws)
-    print (f"{choices[player_choice]} VS {bot_choice}")
-    if choices[player_choice] == bot_choice:
-        print('Tie!')
-        players_results.append('tie')    
-    elif compare(choices[player_choice], bot_choice):
-        print('Player Wins!')
-        players_results.append('win')
+    if len(players_throws) > 5:
+        bot_choice = pattern_matcher()
+        compare_throws(choices[player_choice], bot_choice)
     else:
-        print('Computer Wins!')
-        players_results.append('loss')
-    print('=====================================================')
-    players_throws.append(choices[player_choice].lower())
+        bot_choice = throw_predictor(players_results, players_throws)
+        compare_throws(choices[player_choice], bot_choice)
+    players_throws.append(choices[player_choice])
 
-
-
-def compare(playerChoice, botChoice):
+def compare_throws(player_choice, bot_choice):
     results = {('paper','rock') : True,
                ('paper','scissors') : False,
                ('rock','paper') : False,
                ('rock','scissors') : True,
                ('scissors','paper') : True,
                ('scissors','rock') : False}
-    return results[(playerChoice, botChoice)]
-
+    print (f"{choices[player_choice]} VS {bot_choice}") 
+    if player_choice == bot_choice:
+        print('Tie!')
+        players_results.append('tie')    
+    elif results[(player_choice, bot_choice)]:
+        print('Player Wins!')
+        players_results.append('win')
+    else:
+        print('Computer Wins!')
+        players_results.append('loss')   
+    
 def opposite_throw(last_throw):
     opposite_throw = {
         'paper': 'scissors',
@@ -68,6 +69,32 @@ def throw_predictor(players_results, players_throws):
         smart_bot_choice = choices[random.randint(1,3)]
         return smart_bot_choice
 
+def pattern_matcher():
+    predicted_throws = []
+    for i in range(len(players_throws)-1):
+        len_of_pattern = ((len(players_throws)-i))
+        if len_of_pattern <= len(players_throws)/2:
+            for ix in range(len(players_throws)):
+                start_of_slice = ix
+                end_of_slice = start_of_slice+len_of_pattern
+                if end_of_slice < len(players_throws):
+                    players_throws_to_check_against = slice(start_of_slice, end_of_slice)
+                    if (players_throws[players_throws_to_check_against]) == (players_throws[len(players_throws)-len_of_pattern:len(players_throws)]):
+                        predicted_throws.append(players_throws[end_of_slice])
+    if not predicted_throws:
+        bot_choice = throw_predictor(players_results, players_throws) 
+    else:
+        rock = predicted_throws.count('rock')
+        paper = predicted_throws.count('paper')
+        scissors = predicted_throws.count('scissors')
+
+        if rock > paper and rock > scissors:
+            bot_choice = opposite_throw('rock')
+        elif paper > rock and paper > scissors:
+            bot_choice = opposite_throw('paper')
+        else:
+            bot_choice = opposite_throw('scissors')
+    return bot_choice
 
 rounds = int(input("How many rounds would you like to play? "))
 
@@ -76,7 +103,9 @@ for _ in range(rounds):
     win = players_results.count('win')
     loss = players_results.count('loss')
     tie = players_results.count('tie')
+    print('=====================================================')
     print (f'Wins:{win} Losses:{loss} Ties: {tie}')
+    print('=====================================================')
 
 if loss > win:
     print ('Computer wins')
